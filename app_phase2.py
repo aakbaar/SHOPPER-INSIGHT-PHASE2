88ce4a01e5225b30dc9e9f65db37a7805c1c1112
Muhammad Akbar <muhammadakbar4004@gmail.com>
2026-02-25 04:02:40 +0700

    Update app_phase2.py

--- app_phase2.py
+++ app_phase2.py
@@ -25,6 +25,16 @@ section[data-testid="stSidebar"] {
 html {
     zoom: 100%;
 }
+/* stop dataframe shifting */
+div[data-testid="stDataFrame"] {
+    overflow: auto !important;
+}
+
+/* kunci layout halaman */
+.block-container {
+    padding-top: 1rem !important;
+    padding-bottom: 1rem !important;
+}
 
 /* Jika lebar layar <= 1366px (laptop 13-14 inch) */
 @media screen and (max-width: 1366px) {

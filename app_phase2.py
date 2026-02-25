c6885390a193a3961933931408cf6fc4ecfb94b7
Muhammad Akbar <muhammadakbar4004@gmail.com>
2026-02-25 12:45:54 +0700

    Update app_phase2.py

--- app_phase2.py
+++ app_phase2.py
@@ -1107,7 +1107,7 @@ def main():
         with col_sec: 
             # HAPUS ["ALL"] + dan gunakan index=start_idx
             sel_sec = st.selectbox("SECTION FILTER", sections_only, index=start_idx, key="sec_perf")
-        st.markdown(f"**[JUL - SEP 2025]**")
+        st.markdown(f"**[NOV 2025 - JAN 2026]**")
 
         st.markdown("---")
 
@@ -1291,7 +1291,7 @@ def main():
             st.markdown('<h1 style="margin:0;">🔄 SWITCHING & LOYALTY ANALYSIS</h1>', unsafe_allow_html=True)
         with col_sec_filter:
             sel_sec_sw = st.selectbox("SECTION FILTER", sections_only, index=start_idx, key="sec_sw_top")
-        st.markdown(f"**[JUL - SEP 2025]**")
+        st.markdown(f"**[NOV 2025 - JAN 2026]**")
 
         st.markdown("---")
         loy_data = load_loyalty_data()
@@ -1550,7 +1550,7 @@ def main():
 
         # SETUP OPSI PLANO
         plano_options = ["BEFORE_V1", "BEFORE_V2", "AFTER_V1", "AFTER_V2", "NOT_TRIAL"]
-        st.markdown(f"**[JUL - SEP 2025]**")
+        st.markdown(f"**[NOV 2025 - JAN 2026]**")
         with col_plano_filter:
             sel_plano = st.selectbox(
                 "VERSI PLANO",

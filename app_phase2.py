3bb6fa189733c42ca328b8533c4c71e4731bf18e
Muhammad Akbar <muhammadakbar4004@gmail.com>
2026-03-01 22:52:14 +0700

    Update app_phase2.py

--- app_phase2.py
+++ app_phase2.py
@@ -360,7 +360,7 @@ def render_performance_cards(df, is_category=False):
         background-color: #F8F9FA; 
         border-radius: 8px; 
         padding: 12px 14px; 
-        height: 135px; 
+        height: 105px; 
         border: 1px solid #EEE;
         display: flex;
         flex-direction: column;
